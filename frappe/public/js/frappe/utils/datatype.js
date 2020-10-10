668f74df04e705c632478ff7535fd248edae1fa8
Saif Ur Rehman <saifi0102@gmail.com>
2020-10-10 14:08:40 +0500

    fix: missing utility function from version-11

--- frappe/public/js/frappe/utils/datatype.js
+++ frappe/public/js/frappe/utils/datatype.js
@@ -18,6 +18,14 @@ window.cint = function(v, def) {
 	return v;
 };
 
+window.remove_wrapping_quotes = function(str) {
+	str = cstr(str);
+	if (str.length && str.substr(0, 1) === "'" && str.substr(str.length - 1, 1) === "'") {
+		str = str.substr(1, str.length-2);
+	}
+	return str;
+}
+
 // to title case
 window.toTitle = function(str){
 	var word_in = str.split(" ");
