83c4fb5a28156101972068f191b2a06b40f6e3c7
Saif Ur Rehman <saifi0102@gmail.com>
2020-10-21 22:00:41 +0500

    fix: ToDo Patch reload doctype

--- frappe/patches/v12_0/set_todo_title.py
+++ frappe/patches/v12_0/set_todo_title.py
@@ -1,6 +1,8 @@
 import frappe
 
 def execute():
+	frappe.reload_doctype("ToDo")
+
 	todos = frappe.get_all("ToDo", filters={'title': ['is', 'not set']})
 	for d in todos:
 		doc = frappe.get_doc("ToDo", d.name)
