34ee298bd4f150e11462c49fcf099b7e231e10c5
Saif Ur Rehman <saifi0102@gmail.com>
2020-11-17 13:48:09 +0500

    fix: Do not disable checkbox on submitted document

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -251,7 +251,7 @@ export default class Grid {
 
 		// toolbar
 		this.setup_toolbar();
-		this.toggle_checkboxes(this.display_status !== 'Read');
+		// this.toggle_checkboxes(this.display_status !== 'Read');
 
 		// sortable
 		if(this.frm && this.is_sortable() && !this.sortable_setup_done) {
