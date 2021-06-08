2aedaa6e8ed2001db641078be7383686cb4ad4b2
Saif Ur Rehman <saifi0102@gmail.com>
2021-06-08 14:18:17 +0500

    chore: Reduce form margins and padding

--- frappe/public/js/frappe/form/controls/text.js
+++ frappe/public/js/frappe/form/controls/text.js
@@ -15,6 +15,6 @@ frappe.ui.form.ControlLongText = frappe.ui.form.ControlText;
 frappe.ui.form.ControlSmallText = frappe.ui.form.ControlText.extend({
 	make_input: function() {
 		this._super();
-		this.$input.css({'height': '100px'});
+		this.$input.css({'height': '94px'});
 	}
 });
