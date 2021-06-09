5b2b7f788bec4ac983e65597ce6547e5044c054e
Saif Ur Rehman <saifi0102@gmail.com>
2021-06-10 00:38:38 +0500

    fix(Multiple Link Selector): Set Qty first

--- frappe/public/js/frappe/form/link_selector.js
+++ frappe/public/js/frappe/form/link_selector.js
@@ -152,9 +152,9 @@ frappe.ui.form.LinkSelector = Class.extend({
 							d = me.target.add_new_row();
 						},
 						() => frappe.timeout(0.1),
-						() => frappe.model.set_value(d.doctype, d.name, me.fieldname, value),
-						() => frappe.timeout(0.5),
 						() => frappe.model.set_value(d.doctype, d.name, me.qty_fieldname, data.qty),
+						() => frappe.timeout(0.1),
+						() => frappe.model.set_value(d.doctype, d.name, me.fieldname, value),
 						() => frappe.show_alert(__("Added {0} ({1})", [value, data.qty]))
 					]);
 				}
