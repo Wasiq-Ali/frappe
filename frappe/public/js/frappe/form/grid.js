0819ed03fa77d69306639a8f7934e2fe5489abad
Saif Ur Rehman <saifi0102@gmail.com>
2021-11-18 14:19:03 +0500

    fix(Grid): Duplicate row $.each issue

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -536,12 +536,12 @@ export default class Grid {
 	}
 
 	duplicate_row(d, copy_doc) {
-		$.each(copy_doc, function(key, value) {
+		for (let [key, value] of Object.entries(copy_doc)) {
 			if(!["creation", "modified", "modified_by", "idx", "owner",
 				"parent", "doctype", "name", "parentield"].includes(key)) {
 				d[key] = value;
 			}
-		});
+		}
 
 		return d;
 	}
