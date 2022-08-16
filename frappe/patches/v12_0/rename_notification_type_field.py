bde0fcedb2f6728a213f9824cd2efaab2a695acf
Saif Ur Rehman <saifi0102@gmail.com>
2022-08-16 17:57:06 +0500

    fix(SMS): patch fix

--- frappe/patches/v12_0/rename_notification_type_field.py
+++ frappe/patches/v12_0/rename_notification_type_field.py
@@ -3,5 +3,7 @@ from frappe.model.utils.rename_field import rename_field
 
 
 def execute():
+	frappe.rename_doc("core", "doctype", "sms_template")
+
 	if frappe.db.has_column("SMS Template", "type"):
 		rename_field("SMS Template", "type", "notification_type")
