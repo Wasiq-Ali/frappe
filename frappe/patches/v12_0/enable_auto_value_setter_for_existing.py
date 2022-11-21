574d5be94ad16f9703a745e22441f6f9f40cc308
Saif Ur Rehman <saifi0102@gmail.com>
2022-11-21 16:35:42 +0500

    fix: reload Auto Value Setter doctype in patch

--- frappe/patches/v12_0/enable_auto_value_setter_for_existing.py
+++ frappe/patches/v12_0/enable_auto_value_setter_for_existing.py
@@ -1,13 +1,15 @@
 import frappe
 
 def execute():
-    doc_type_list = frappe.db.sql_list("""select distinct document_type from `tabAuto Value Setter`""")
-	
-    for doc_type in  doc_type_list:
-        frappe.make_property_setter({
-            'doctype': doc_type,
-            'doctype_or_field': 'DocType',
-            'property': 'allow_auto_value_setter',
-            'property_type': 'Check',
-            'value': 1
-        })
+	frappe.reload_doc("core", "doctype", "auto_value_setter")
+
+	doc_type_list = frappe.db.sql_list("""select distinct document_type from `tabAuto Value Setter`""")
+
+	for doc_type in  doc_type_list:
+		frappe.make_property_setter({
+			'doctype': doc_type,
+			'doctype_or_field': 'DocType',
+			'property': 'allow_auto_value_setter',
+			'property_type': 'Check',
+			'value': 1
+		})
