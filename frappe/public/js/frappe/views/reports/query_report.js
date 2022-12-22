6b8db1fd4be5eab3d61334cbe92e30f7b9036004
Saif Ur Rehman <saifi0102@gmail.com>
2022-12-22 18:01:56 +0500

    feat(Query Report): check get items util

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1862,6 +1862,11 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		return items;
 	}
 
+	get_checked_items() {
+		const indexes = this.datatable.rowmanager.getCheckedRows();
+		return indexes.map(i => this.data[i]).filter(i => i != undefined);
+	}
+
 	add_portrait_warning(dialog) {
 		if (this.columns.length > 10) {
 			dialog.set_df_property("orientation", "change", () => {
