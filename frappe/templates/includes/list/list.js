71f567be574c1c1c052514b1a31bcf8c7d0554db
Imran Ali <imranbhutto146@gmail.com>
2023-02-06 18:42:10 +0500

    feat(list.py): added show more btn

--- frappe/templates/includes/list/list.js
+++ frappe/templates/includes/list/list.js
@@ -1,45 +1,118 @@
-frappe.ready(function() {
-	var next_start = {{ next_start or 0 }};
-	var result_wrapper = $(".website-list .result");
+window.list_view = {
+	next_start: {{ next_start or 0 }},
+	loading: false,
+}
 
-	$(".website-list .btn-more").on("click", function() {
-		var btn = $(this);
-		var data = $.extend(frappe.utils.get_query_params(), {
-			doctype: "{{ doctype }}",
-			txt: "{{ txt or '' }}",
-			limit_start: next_start,
-			pathname: location.pathname,
+$.extend(list_view, {
+	bind_events: function() {
+		$(".website-list .btn-more").on("click", function() {
+			let btn = $(this);
+			return list_view.load_more(btn);
 		});
-		data.web_form_name = frappe.web_form_name;
-		data.pathname = location.pathname;
-		btn.prop("disabled", true);
+
+		if($('.navbar-header .navbar-toggle:visible').length === 1) {
+			$('.page-head h1').addClass('list-head').click(function(){
+				window.history.back();
+			 });
+		}
+	},
+
+	reload: function(opts) {
+		if (list_view.loading) {
+			return;
+		}
+
+		if (!opts) {
+			opts = {};
+		}
+
+		let args = list_view.get_query_args();
+
+		list_view.set_is_loading(true, opts.btn);
+
 		return $.ajax({
-			url:"/api/method/frappe.www.list.get",
-			data: data,
+			url: "/api/method/frappe.www.list.get",
+			data: args,
 			statusCode: {
-				200: function(data) {
-					var data = data.message;
-					next_start = data.next_start;
-					$.each(data.result, function(i, d) {
-						$(d).appendTo(result_wrapper);
-					});
-					toggle_more(data.show_more);
+				200: function(r) {
+					list_view.next_start = r.message.next_start;
+
+					list_view.clear_list();
+					list_view.append_list(r.message.result);
+					list_view.toggle_show_more_button(r.message.show_more);
+
+					opts.callback && opts.callback(r);
 				}
 			}
 		}).always(function() {
-			btn.prop("disabled", false);
+			list_view.set_is_loading(false, opts.btn);
 		});
-	});
-	var toggle_more = function(show) {
-		if (!show) {
-			$(".website-list .more-block").addClass("hide");
-		}
-	};
+	},
 
-	if($('.navbar-header .navbar-toggle:visible').length === 1)
-	{
-		$('.page-head h1').addClass('list-head').click(function(){
-			window.history.back();
-	 	});
+	load_more: function(btn) {
+		if (list_view.loading) {
+			return;
+		}
+
+		let args = list_view.get_query_args();
+		args["limit_start"] = list_view.next_start;
+
+		list_view.set_is_loading(true, btn);
+
+		return $.ajax({
+			url: "/api/method/frappe.www.list.get",
+			data: args,
+			statusCode: {
+				200: function(r) {
+					list_view.next_start = r.message.next_start;
+
+					list_view.append_list(r.message.result);
+					list_view.toggle_show_more_button(r.message.show_more);
+				}
+			}
+		}).always(function() {
+			list_view.set_is_loading(false, btn);
+		});
+	},
+
+	get_query_args: function() {
+		return $.extend(frappe.utils.get_query_params(), {
+			doctype: "{{ doctype }}",
+			txt: "{{ txt or '' }}",
+			pathname: location.pathname,
+			web_form_name: frappe.web_form_name,
+		});
+	},
+
+	clear_list: function() {
+		list_view.result_wrapper.empty();
+	},
+
+	append_list: function(rows) {
+		$.each(rows || [], function(i, d) {
+			list_view.append_row(d)
+		});
+	},
+
+	append_row: function(row_html) {
+		if (row_html) {
+			$(row_html).appendTo(list_view.result_wrapper);
+		}
+	},
+
+	toggle_show_more_button: function(show) {
+		$(".website-list .more-block").toggleClass("hide", !show);
+	},
+
+	set_is_loading: function(is_loading, btn) {
+		list_view.loading = Boolean(is_loading);
+		if (btn) {
+			$(btn).prop("disabled", list_view.loading);
+		}
 	}
+});
+
+frappe.ready(function() {
+	list_view.result_wrapper = $(".website-list .result");
+	list_view.bind_events();
 });
