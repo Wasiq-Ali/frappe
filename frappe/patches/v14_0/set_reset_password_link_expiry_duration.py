238612d1e3ab49949ea71343bd715ad7795c8401
Saif Ur Rehman <saif@paralogic.io>
2024-03-29 14:14:49 +0500

    fix(System Settings): default password reset duration as 1 day

---
+++ frappe/patches/v14_0/set_reset_password_link_expiry_duration.py
@@ -0,0 +1,6 @@
+import frappe
+
+
+def execute():
+	if frappe.db.get_single_value("System Settings", "reset_password_link_expiry_duration", cache=False):
+		frappe.db.set_value("System Settings", None, "reset_password_link_expiry_duration", 86400)
