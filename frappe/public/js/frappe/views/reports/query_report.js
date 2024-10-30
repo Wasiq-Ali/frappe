aa4f878ed64e48c8354f3a6c0120a675a5a94c84
Saif Ur Rehman <saif@paralogic.io>
2024-10-30 23:06:30 +0500

    fix(Query Report): fix footer

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -2124,29 +2124,29 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 
 	show_footer_message() {
 		this.$report_footer && this.$report_footer.remove();
-		this.$report_footer = $(`<div class="report-footer text-muted"></div>`).appendTo(
-			this.page.main
-		);
+		this.$report_footer = $(`<div class="report-footer text-muted">
+			<div class="report-footer-items"></div>
+		</div>`).appendTo(this.page.main);
+
+		this.$report_footer_items = $(".report-footer-items", this.$report_footer);
+
 		if (this.tree_report) {
-			this.$tree_footer = $(`<div class="tree-footer col-md-6">
+			this.$tree_footer = $(`<div class="tree-footer">
 				<button class="btn btn-xs btn-default" data-action="expand_all_rows">
 					${__("Expand All")}</button>
 				<button class="btn btn-xs btn-default" data-action="collapse_all_rows">
 					${__("Collapse All")}</button>
 			</div>`);
-			$(this.$report_footer).append(this.$tree_footer);
-			this.$tree_footer.find("[data-action=collapse_all_rows]").show();
-			this.$tree_footer.find("[data-action=expand_all_rows]").hide();
+			this.$report_footer_items.append(this.$tree_footer);
 		}
 
 		const message = __(
 			"For comparison, use >5, <10 or =324. For ranges, use 5:10 (for values between 5 & 10)."
 		);
-		const execution_time_msg = __("Execution Time: {0} sec", [this.execution_time || 0.1]);
+		const execution_time_msg = __("Execution Time: {0} sec", [flt(this.execution_time, 3) || 0.1]);
 
-		this.$report_footer.append(`<div class="col-md-12">
-			<span">${message}</span><span class="pull-right">${execution_time_msg}</span>
-		</div>`);
+		this.$report_footer_items.append(`<div>${message}</div>`);
+		this.$report_footer_items.append(`<div>${execution_time_msg}</div>`);
 	}
 
 	expand_all_rows() {
