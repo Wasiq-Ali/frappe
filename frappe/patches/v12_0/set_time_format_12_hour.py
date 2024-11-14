ff6c0ab66f1e460c919ca956d0822a8a5dc4b22d
Saif Ur Rehman <saif@paralogic.io>
2024-11-14 13:52:32 +0500

    fix: replace with frappe.db.set_single_value

--- frappe/patches/v12_0/set_time_format_12_hour.py
+++ frappe/patches/v12_0/set_time_format_12_hour.py
@@ -3,5 +3,5 @@ import frappe
 
 def execute():
 	frappe.reload_doctype("System Settings")
-	frappe.db.set_value("System Settings", None, "time_format", "12 Hour")
+	frappe.db.set_single_value("System Settings", "time_format", "12 Hour")
 	frappe.db.set_default("time_format", "12 Hour")
--- frappe/patches/v14_0/set_reset_password_link_expiry_duration.py
+++ frappe/patches/v14_0/set_reset_password_link_expiry_duration.py
@@ -3,4 +3,4 @@ import frappe
 
 def execute():
 	if frappe.db.get_single_value("System Settings", "reset_password_link_expiry_duration", cache=False):
-		frappe.db.set_value("System Settings", None, "reset_password_link_expiry_duration", 86400)
+		frappe.db.set_single_value("System Settings", "reset_password_link_expiry_duration", 86400)
