29d1b79908de874ea69112a174c7d5e333cdeb4c
MAunZaidi <aunzaidi118@gmail.com>
2024-11-25 17:34:07 +0500

    chore(regional): Fixed indentation

--- frappe/regional/regional.js
+++ frappe/regional/regional.js
@@ -5,13 +5,13 @@ import "./uae.js";
 
 Object.assign(frappe.regional, {
 	get_formatted_mobile_nos : function(value){
-		if (frappe.sys_defaults.country == "Pakistan"){
+		if (frappe.sys_defaults.country == "Pakistan") {
 			return frappe.regional.pakistan.get_formatted_mobile_no(value);
 		}
-		else if (frappe.sys_defaults.country == "United Arab Emirates"){
+		else if (frappe.sys_defaults.country == "United Arab Emirates") {
 			return frappe.regional.uae.get_formatted_uae_mobile_no(value);
 		}
-		else{
+		else {
 			return Boolean(value)
 		}
 
