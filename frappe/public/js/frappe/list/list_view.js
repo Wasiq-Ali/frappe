ccd799bad16ff2c7a737fee7e82cd4dbe98a8c71
Saif Ur Rehman <saif@paralogic.io>
2024-12-21 16:37:19 +0500

    fix(ToDo List): error on button class

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -946,7 +946,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 		if (!this.hide_row_button && this.settings.button && this.settings.button.show(doc)) {
 			settings_button = `
 				<span class="list-actions">
-					<button class="btn btn-action btn-xs ${this.settings.button?.get_class(doc) || "btn-default"}"
+					<button class="btn btn-action btn-xs ${this.settings.button?.get_class?.(doc) || "btn-default"}"
 						data-name="${doc.name}" data-idx="${doc._idx}"
 						title="${this.settings.button.get_description(doc)}">
 						${this.settings.button.get_label(doc)}
