5ee0dda850c05b7c4ea1255c352aa3ca68a8ad87
Saif Ur Rehman <saif@paralogic.io>
2025-06-04 12:52:24 +0500

    fix(pdf): use lazy regex for options extraction

--- frappe/utils/pdf.py
+++ frappe/utils/pdf.py
@@ -224,7 +224,7 @@ def read_options_from_html(html):
 	)
 	for attr in attrs:
 		try:
-			pattern = re.compile(r"(\.print-format)([\S|\s][^}]*?)(" + str(attr) + r":)(.+)(mm;)")
+			pattern = re.compile(r"(\.print-format)([\S|\s][^}]*?)(" + str(attr) + r":)(.*?)(mm;)")
 			match = pattern.findall(html)
 			if match:
 				options[attr] = str(match[-1][3]).strip()
